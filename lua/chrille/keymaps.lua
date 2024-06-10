d95eba801ad06b0a6ab5506d6d8f839eed6bc21c
chrille0234 <madsen.chr@hotmail.com>
2024-06-10 20:49:01 +0200

    fix spelling error (customScripts -> Custom_scripts)

--- lua/chrille/keymaps.lua
+++ lua/chrille/keymaps.lua
@@ -5,4 +5,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ex", vim.cmd.exit)
 
 
-vim.api.nvim_set_keymap('n', '<Leader>tcc', '<cmd>lua customScripts.switch_case()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>tcc', '<cmd>lua Custom_scripts.switch_case()<CR>', {noremap = true, silent = true})
